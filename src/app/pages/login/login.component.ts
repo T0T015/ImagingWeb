c46957b8cd70326c58e742d7823ea1bcdd5e2d5d
Beto98 <muzaurietabeto@gmail.com>
2023-05-22 22:02:17 -0500

    En Imaging 22-05-23

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { MatSnackBar} from '@angular/material/snack-bar';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  loginData = {
+    "username" : '',
+    "password" : ''
+  }
+  
+  constructor(private snack:MatSnackBar, private loginService:LoginService){ }
+
+  ngOnInit(): void {
+      
+  }
+
+  formSubmit(){
+    if(this.loginData.username.trim() == '' || this.loginData.username.trim() == null){
+      Swal.fire('Advertencia','El nombre de usuario es requerido','warning');
+      return;
+    }
+    if(this.loginData.password.trim() == '' || this.loginData.password.trim() == null){
+      Swal.fire('Advertencia','La contraseña es requerida','warning');
+      return;
+    }
+
+    this.loginService.generateToken(this.loginData).subscribe(
+      (data:any) =>{
+        console.log(data);
+
+        this.loginService.loginUser(data.token);
+        this.loginService.getCurrentUser().subscribe((user:any) => {
+          console.log(user);
+        }) 
+      },(error) => {
+        console.log(error);
+      }
+    )
+  }
 
 }
